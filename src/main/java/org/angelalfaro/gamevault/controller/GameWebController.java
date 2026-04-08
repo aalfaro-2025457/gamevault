d233154c6a8179f9bb8c3796f1eedced81fc16d2
aalfaro-2025457 <aalfaro-2025457@kinal.edu.gt>
2026-04-07 20:34:21 -0600

    refactor: chamging frontend React to thymeleaf

--- src/main/java/org/angelalfaro/gamevault/controller/GameWebController.java
+++ src/main/java/org/angelalfaro/gamevault/controller/GameWebController.java
@@ -1,6 +1,8 @@
 package org.angelalfaro.gamevault.controller;
 
 import lombok.RequiredArgsConstructor;
+
+import org.angelalfaro.gamevault.entity.Category;
 import org.angelalfaro.gamevault.entity.Game;
 import org.angelalfaro.gamevault.entity.User;
 import org.angelalfaro.gamevault.service.CategoryService;
@@ -11,7 +13,9 @@ import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequiredArgsConstructor
 @Controller
@@ -22,14 +26,29 @@ public class GameWebController {
     private final UserService userService;
     private final CategoryService categoryService;
 
+    // Endpoint to get the lis of games
     @GetMapping("/games")
     public String listGames(Model model, Pageable pageable) {
         User defaultUser = userService.getOrCreateDefaultUser("Angel", "1234");
-
         Slice<Game> gameSlice = gameService.listUserGames(defaultUser.getIdUser(), pageable);
 
         model.addAttribute("games", gameSlice.getContent());
         model.addAttribute("hasNext", gameSlice.hasNext());
-        return "games-list"; // Search src/main/resources/templates/games-list.html
+        model.addAttribute("currentPage", pageable.getPageNumber());
+        return "games-list"; 
+    }
+
+    // end point to use in the form to add a new game
+    @PostMapping("/games/add")
+    public String addGame(@RequestParam String title,
+                          @RequestParam String categoryName,
+                          @RequestParam String imageUrl) {
+        
+        User user = userService.getOrCreateDefaultUser("Angel", "1234");
+        Category category = categoryService.getOrCreateCategory(categoryName);
+        
+        gameService.saveGame(title, user, category, imageUrl);
+        
+        return "redirect:/admin/games";
     }
 }
